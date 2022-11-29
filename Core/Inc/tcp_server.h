948739ee7065de76443956c37d5931c252dc1e8a
Wojciech Kloc <wojtek327@gmail.com>
2022-11-29 14:47:26 +0100

    V1.0 lwip tcp/http server

---
+++ Core/Inc/tcp_server.h
@@ -0,0 +1,13 @@
+/*
+ * tcp_server.h
+ *
+ *  Created on: 11 gru 2020
+ *      Author: wojte
+ */
+
+#ifndef INC_TCP_SERVER_H_
+#define INC_TCP_SERVER_H_
+
+void tcp_echoserver_init(void);
+
+#endif /* INC_TCP_SERVER_H_ */
---
+++ README.md
@@ -0,0 +1,6 @@
+# STM32H753ZI---lwip_tcp_http_server
+
+STM32H7 lwip tcp http server
+
+Blog [PL]:
+https://elektronika327.blogspot.com/2020/12/stm32h7-stm32h753zi-lwip.html
